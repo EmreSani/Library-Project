4a3f8fc427d4ebeecf5b8954b5bee9c23133dd12
EmreSani <emresani1998@gmail.com>
2024-06-12 14:08:57 +0300

    first commit

---
+++ src/main/java/com/dev02/libraryproject/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dev02.libraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev02/libraryproject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev02.libraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
