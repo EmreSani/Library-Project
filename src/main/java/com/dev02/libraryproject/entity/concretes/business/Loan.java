76adbd6dbdbe1a883a04146998f8c069830862d2
Osman Şeyban <osmanseyban@gmail.com>
2024-06-13 21:09:37 +0300

    Removed embedded repository

--- src/main/java/com/dev02/libraryproject/entity/concretes/business/Loan.java
+++ src/main/java/com/dev02/libraryproject/entity/concretes/business/Loan.java
@@ -1,8 +1,37 @@
 package com.dev02.libraryproject.entity.concretes.business;
 
-<<<<<<< HEAD
-=======
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
->>>>>>> main
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Long userId;
+
+    @ManyToOne
+    @JsonIgnore
+    private Long bookId;
+
+    private LocalDateTime expireDate;
+
+    private LocalDateTime returnDate;
+
+    private String notes;
 }
