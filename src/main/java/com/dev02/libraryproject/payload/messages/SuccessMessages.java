10f7124d1d52a47357b8673cf9929d0a44d69635
EmreSani <emresani1998@gmail.com>
2024-06-15 14:43:02 +0300

    succes messages fixed

--- src/main/java/com/dev02/libraryproject/payload/messages/SuccessMessages.java
+++ src/main/java/com/dev02/libraryproject/payload/messages/SuccessMessages.java
@@ -5,9 +5,12 @@ public class SuccessMessages {
     private SuccessMessages() {
     }
 
-    public static final String SUCCESS = "Completed Successfully"
+    public static final String AUTHOR_FOUND = "Author Found..." ;
+    public static final String AUTHOR_CREATED = "Author Created Successfully...";
+    public static final String AUTHOR_UPDATED = "Author Updated Successfully...";
+    public static final String AUTHOR_DELETED = "Author Deleted Successfully";
 
-    public static final String AUTHOR_FOUND = "Author Found." ;
+    public static final String SUCCESS = "Completed Successfully";
 
     public static final String USER_FOUND = "USER Found." ;
 
