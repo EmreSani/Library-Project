0654930e8737cb8d8b147eceb051e38f89d89f69
EmreSani <emresani1998@gmail.com>
2024-06-16 01:08:11 +0300

    publisher end points started

--- src/main/java/com/dev02/libraryproject/controller/business/PublisherController.java
+++ src/main/java/com/dev02/libraryproject/controller/business/PublisherController.java
@@ -1,4 +1,16 @@
 package com.dev02.libraryproject.controller.business;
 
+import com.dev02.libraryproject.service.business.PublisherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publishers")
+@RequiredArgsConstructor
 public class PublisherController {
+    private final PublisherService publisherService;
+
+
+
 }
