9c478ca29b07d846af4d68a7e0383ec6bcb32edc
hakankaratay <hakankaratay1991@gmail.com>
2024-06-17 21:35:05 +0300

    loan methods fixed

--- src/main/java/com/dev02/libraryproject/payload/messages/ErrorMessages.java
+++ src/main/java/com/dev02/libraryproject/payload/messages/ErrorMessages.java
@@ -7,7 +7,7 @@ public class ErrorMessages {
 
     public static final String ROLE_NOT_FOUND = "Role doesn't exist";
 
-    public static final String AUTHOR_NOT_FOUND="Author is not found by id : %s.";
+    public static final String AUTHOR_NOT_FOUND="Author is not found by id : %s";
     public static final String BOOK_NOT_LOANABLE="Book is not loanable";
 
     public static final String ALREADY_REGISTER_MESSAGE_EMAIL= "Email is exists already : %s";
@@ -18,7 +18,8 @@ public class ErrorMessages {
     public static final String USER_CAN_NOT_LOAN = "User can not loan this book now. Check loanlist and score";
     public static final String NOT_FOUND_USER_MESSAGE = "Error: User not found with id %s";
 
-    public static final String LOAN_NOT_FOUND="Loan is not found by id : %s.";
+    public static final String LOAN_NOT_FOUND="Loan is not found by id : %s";
+    public static final String LOAN_NOT_FOUND_BY_USER="This loan does not belong to this user id: %s";
 
 
 
