27cf91d7a7ec4f2e6b05993bf74f8884b20bb89d
EmreSani <emresani1998@gmail.com>
2024-06-18 23:40:20 +0300

    1 import fixed

--- src/main/java/com/dev02/libraryproject/entity/concretes/business/Publisher.java
+++ src/main/java/com/dev02/libraryproject/entity/concretes/business/Publisher.java
@@ -4,12 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
