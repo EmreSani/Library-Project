642305bd30cb8ba4c1ffcc69c0ba7a389660bbe2
Osman Şeyban <osmanseyban@gmail.com>
2024-06-19 17:03:43 +0300

    Book and Author are finished...!

--- src/main/java/com/dev02/libraryproject/controller/business/BookController.java
+++ src/main/java/com/dev02/libraryproject/controller/business/BookController.java
@@ -6,8 +6,6 @@ import com.dev02.libraryproject.payload.response.business.ResponseMessage;
 import com.dev02.libraryproject.service.business.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +23,14 @@ public class BookController {
 
 
     @GetMapping("/q")
+
     // http://localhost:8080/books?q=sefiller&cat=4&author=34&publisher=42&page=1&size=10&sort=name&type=asc
     public Page<BookResponse> getAllBookByPage(
+            HttpServletRequest httpServletRequest,
             @RequestParam(name = "q", defaultValue = "sefiller") String query,
-            @RequestParam(name = "cat", defaultValue = "4") Integer category,
-            @RequestParam(name = "author", defaultValue = "34") Long author,
-            @RequestParam(name = "publisher", defaultValue = "42") Integer publisher,
+            @RequestParam(name = "cat", defaultValue = "4") Long categoryId,
+            @RequestParam(name = "author", defaultValue = "34") Long authorId,
+            @RequestParam(name = "publisher", defaultValue = "42") Long publisherId,
             @RequestParam(name = "page", defaultValue = "1") Integer page,
             @RequestParam(name = "size", defaultValue = "10") Integer size,
             @RequestParam(name = "sort", defaultValue = "name") String sort,
@@ -41,7 +41,7 @@ public class BookController {
 
 
     @GetMapping("{/id}")
-    public ResponseMessage<BookResponse> findBookById(@RequestParam Long id) {
+    public ResponseMessage<BookResponse> getBookById(@RequestParam Long id) {
 
         return bookService.findBookById(id);
 
@@ -63,9 +63,8 @@ public ResponseMessage<BookResponse> updateBook(HttpServletRequest httpServletRe
     }
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public  ResponseMessage<BookResponse> deleteBook(HttpServletRequest httpServlet,
-                                                     @PathVariable Long bookId){
-        return bookService.deleteBook(httpServlet, bookId);
+    public  ResponseMessage<BookResponse> deleteBook(@PathVariable Long bookId){
+        return bookService.deleteBook(bookId);
     }
 }
 
