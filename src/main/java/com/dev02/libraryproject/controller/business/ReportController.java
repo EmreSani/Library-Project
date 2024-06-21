cccf7f7c28de26b0ae231b314f4b193db4d01a4f
Şadiye Şahin <sadiye_sahn@hotmail.com>
2024-06-21 13:27:59 +0300

    unreturnedBooks

--- src/main/java/com/dev02/libraryproject/controller/business/ReportController.java
+++ src/main/java/com/dev02/libraryproject/controller/business/ReportController.java
@@ -3,15 +3,13 @@ package com.dev02.libraryproject.controller.business;
 import com.dev02.libraryproject.payload.response.business.BookResponse;
 import com.dev02.libraryproject.payload.response.business.BookResponseForReport;
 import com.dev02.libraryproject.payload.response.business.LoanResponse;
+import com.dev02.libraryproject.payload.response.business.ResponseMessage;
 import com.dev02.libraryproject.service.business.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,5 +29,20 @@ public class ReportController {
             @RequestParam(value = "type", defaultValue = "desc") String type) {
         return reportService.getAllExpiredBooksByPage(page, size, sort, type);
     }
+
+    //3.endpoint
+    // http://localhost:8080/report/unreturned-books? page=1&size=10&sort=expireDate&type=desc
+    @PostMapping("/report/unreturned-books")
+    @PreAuthorize("hasAnyAuthority('ADMIN','EMPLOYEE')")
+    public ResponseEntity<Page<BookResponseForReport>> getAllUnreturnedBooksByPage(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "20") int size,
+            @RequestParam(value = "sort", defaultValue = "expireDate") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type) {
+
+        return reportService.getAllUnreturnedBooksByPage(page, size, sort, type);
+    }
+
+
 
 }
